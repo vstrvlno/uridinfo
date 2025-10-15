68bfbea2616867090f601cfd7671b055f8a8a9b2
vstrvlno <tair.kz.fg@gmail.com>
2025-10-15 11:38:08 +0500

    Create __init__.py

---
+++ parsers/__init__.py
@@ -0,0 +1,6 @@
+# parsers/__init__.py
+from .stat_api import search_by_bin, search_by_name, get_company_by_bin
+from .kgd_api import get_tax_info
+from .court_api import get_court_cases
+from .zakup_api import get_goszakup_info
+from .licenses_api import get_licenses_info
